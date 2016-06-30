a0db7af69463d5fecbd883140c12c4cd63b27a67
Michael Arefyev <martcsusa@gmail.com>
2016-06-30 10:37:09 -0400

    Update best.R

--- best.R
+++ best.R
@@ -3,14 +3,15 @@ best <- function (state, outcome){
   ## and returns a character vector with the name of hospital
   ## that has the best (lowest) 30-day mortalityfor the 
   ## specified outcome in than state
-  
-  ## 1. Initialize, create reference data
+
+  ## 1. Befor validating parameters, initialize, create reference data
   
   ocolid       <- as.numeric()
   st_list      <- state.abb
   outcome_list <- c("heart attack","heart failure","pneumonia")
   oframe       <- data.frame(outcome=c("heart attack","heart failure","pneumonia"),
                              colid=c(11,17,23))
+                             
   ## 2. Validate parameters
   
   if(missing(state) | !is.element(state,st_list) )
@@ -19,12 +20,11 @@ best <- function (state, outcome){
     stop("invalid outcome")
   
   ## 3. Main loop - read, validate and extract data
-  
+  temp_ds <- read.csv("week4/outcome-of-care-measures.csv", colClasses="character")
   for (i in 1:nrow(oframe)){
     if(oframe$outcome[i] == outcome) {
       ocolid  <- oframe$colid[i]
-      temp_ds <- read.csv("week4/outcome-of-care-measures.csv", colClasses="character")
-      
+
       ## apply criteria
       ext_ds  <- temp_ds[which(
                                temp_ds$State == state
@@ -32,13 +32,13 @@ best <- function (state, outcome){
                                & temp_ds[,ocolid] != "Not Available"
                                ),]
       
-      ## rename 30-day mortality column
-      names(ext_ds)[ocolid] <- "outcome"
     }
   }
+  ## rename 30-day mortality column to simplify reference
+  names(ext_ds)[ocolid] <- "outcome"
   
   ## 4. Return hospital name with LOWEST outcome value 
-  sub <- which.min(ext_ds$outcome)
-  ext_ds[sub,"Hospital.Name"]
+  ix <- which.min(ext_ds$outcome)
+  ext_ds[ix,"Hospital.Name"]
 }
   
